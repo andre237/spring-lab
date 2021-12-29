eabe372885e751a82c81b5e1caba1ea89ae9f5d4
André Tibola <andre.spironello12@gmail.com>
2021-12-29 17:30:57 -0300

    refactor: remove unchecked casts by overriding with json parse with BaseSchema class

--- src/main/java/com/training/spring/lab/common/schemas/OrderItem.java
+++ src/main/java/com/training/spring/lab/common/schemas/OrderItem.java
@@ -4,6 +4,8 @@ import com.training.spring.lab.common.annotation.KafkaPayload;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.util.Date;
+
 @Data
 @EqualsAndHashCode(callSuper = true)
 @KafkaPayload(identifier = "__Order_Item__")
@@ -11,6 +13,6 @@ public class OrderItem extends BaseSchema {
 
     private String requesterId;
     private String description;
-    private String deadline;
+    private Date deadline;
 
 }
--- src/main/java/com/training/spring/lab/utils/CustomKafkaJsonDeserializer.java
+++ src/main/java/com/training/spring/lab/utils/CustomKafkaJsonDeserializer.java
@@ -3,6 +3,7 @@ package com.training.spring.lab.utils;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.training.spring.lab.common.annotation.KafkaPayload;
+import com.training.spring.lab.common.schemas.BaseSchema;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.reflections.Reflections;
@@ -11,7 +12,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j @Component
-public class CustomKafkaJsonDeserializer<T> implements Deserializer<T> {
+public class CustomKafkaJsonDeserializer implements Deserializer<BaseSchema> {
 
     private final ObjectMapper jsonParser = new ObjectMapper();
     private final HashMap<String, Class<?>> mappedSchemas = new HashMap<>();
@@ -19,20 +20,22 @@ public class CustomKafkaJsonDeserializer<T> implements Deserializer<T> {
     @Override
     public void configure(Map<String, ?> configs, boolean isKey) {
         Reflections reflections = new Reflections("com.training.spring.lab");
-        reflections.getTypesAnnotatedWith(KafkaPayload.class).forEach(type ->
-                mappedSchemas.put(type.getAnnotation(KafkaPayload.class).identifier(), type)
-        );
+        reflections.getTypesAnnotatedWith(KafkaPayload.class).stream()
+                .filter(BaseSchema.class::isAssignableFrom)
+                .forEach(type ->
+                    mappedSchemas.put(type.getAnnotation(KafkaPayload.class).identifier(), type)
+                );
     }
 
     @Override
-    public T deserialize(String topic, byte[] payload) {
+    public BaseSchema deserialize(String topic, byte[] payload) {
         try {
             JsonNode rootNode = jsonParser.readTree(payload);
             JsonNode payloadNode = rootNode.get("data");
 
             Class<?> identifier = mappedSchemas.get(rootNode.get("identifier").asText());
             if (identifier != null && payloadNode != null) {
-                return (T) jsonParser.treeToValue(payloadNode, identifier);
+                return (BaseSchema) jsonParser.treeToValue(payloadNode, identifier);
             }
 
             log.error("Could not find candidate schema to deserialization. Payload = {}", rootNode);
