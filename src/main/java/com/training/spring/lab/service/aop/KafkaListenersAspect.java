7471dbfaba2e101ac97f2a1347c56a5ba1d94b4a
André Tibola <andre.spironello12@gmail.com>
2021-12-29 17:44:48 -0300

    feat: add execution time measuring to kafka aspect

--- src/main/java/com/training/spring/lab/service/aop/KafkaListenersAspect.java
+++ src/main/java/com/training/spring/lab/service/aop/KafkaListenersAspect.java
@@ -1,18 +1,26 @@
 package com.training.spring.lab.service.aop;
 
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Aspect @Component
 public class KafkaListenersAspect {
 
-    @Before("@annotation(org.springframework.kafka.annotation.KafkaListener)")
-    public void notifyListenerTriggered(JoinPoint jp) {
-        log.info("Some kafka consumer has been triggered | Payload = {}", jp.getArgs()[0]);
+    @Around("@annotation(org.springframework.kafka.annotation.KafkaListener)")
+    public Object notifyListenerTriggered(ProceedingJoinPoint pjp) throws Throwable {
+        long startTime = System.nanoTime();
+        Object object = pjp.proceed();
+        double execTime =  (System.nanoTime() - startTime) / 1e6;
+
+        ConsumerRecord<String, ?> payload = (ConsumerRecord<String, ?>) pjp.getArgs()[0];
+        log.info("Kafka consumer triggered. Schema = {} | Execution = {}ms", payload.value().getClass().getSimpleName(), execTime);
+
+        return object;
     }
 
 }
